960a4a8c22f9a2de2393d89ab110110cf342ebac
Pavel Chupin <pavel.chupin@gmail.com>
2024-07-03 17:58:04 +0700

    Убраны не нужные каменты

--- hw10-mvc-rest/src/main/java/ru/otus/hw/controllers/PageBookController.java
+++ hw10-mvc-rest/src/main/java/ru/otus/hw/controllers/PageBookController.java
@@ -17,30 +17,16 @@ public class PageBookController {
 
     @GetMapping("/create/book")
     public String createBookPage(Model model) {
-//        final List<GenreDto> genreDtos = genreService.findAll();
-//        final List<AuthorDto> authorDtos = authorService.findAll();
-//        model.addAttribute("genres", genreDtos);
-//        model.addAttribute("authors", authorDtos);
         return "create";
     }
 
     @GetMapping("/edit/book/{id}")
     public String editPage(@PathVariable("id") Long id, Model model) {
-        //    final BookDto bookDto = bookService.findById(id);
-//        final List<GenreDto> genreDtos = genreService.findAll();
-//        final List<AuthorDto> authorDtos = authorService.findAll();
-
-        //      model.addAttribute("book", bookDto);
-//        model.addAttribute("genres", genreDtos);
-//        model.addAttribute("authors", authorDtos);
-
         return "edit";
     }
 
     @GetMapping("/delete/book/{id}")
     public String deletePage(@PathVariable("id") Long id, Model model) {
-        //   final BookDto bookDto = bookService.findById(id);
-        // model.addAttribute("book", bookDto);
         return "delete";
     }
 }
