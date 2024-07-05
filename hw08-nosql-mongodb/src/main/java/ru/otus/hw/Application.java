1a89b0c3abc29dfb661c85605a39f837a4d10a12
Pavel Chupin <pavel.chupin@gmail.com>
2024-07-05 19:31:35 +0700

    заменен порт для тестов

--- hw08-nosql-mongodb/src/main/java/ru/otus/hw/Application.java
+++ hw08-nosql-mongodb/src/main/java/ru/otus/hw/Application.java
@@ -4,10 +4,9 @@ import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
+//@EnableMongoRepositories
 @EnableMongock
 @EnableConfigurationProperties
 public class Application {
